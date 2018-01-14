a0dba1143d7b77613bb2e3be22edfdcb91e9c047
Viterbo <Viterbo@users.noreply.github.com>
2018-01-14 18:54:58 -0300

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -19,7 +19,7 @@ sudo apt-get upgrade
  
 # --------------
 sudo apt-get install apache2 -y
-sudo apt-get install php7.1 php7.1-cli php7.1-common php7.1-curl php7.1-gd php7.1-json php7.1-mbstring php7.1-mcrypt php7.1-mysql php7.1-opcache php7.1-readline php7.1-zip -y
+sudo apt-get install php7.1 php7.1-cli php7.1-common php7.1-curl php7.1-xml php7.1-gd php7.1-json php7.1-mbstring php7.1-mcrypt php7.1-mysql php7.1-opcache php7.1-readline php7.1-zip -y
  
 sudo a2enmod rewrite
 sudo a2enmod headers
