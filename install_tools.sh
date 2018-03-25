cd87a59d632566b57aa6400929de07af07403584
Viterbo <Viterbo@users.noreply.github.com>
2018-03-25 12:20:54 -0300

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -19,8 +19,8 @@ sudo add-apt-repository -y ppa:ethereum/ethereum -y
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
  
 # --------------
 sudo apt-get install apache2 -y
