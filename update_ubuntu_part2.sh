a6966834c710d2e06419644873d165409842a984
Viterbo <Viterbo@users.noreply.github.com>
2018-04-02 04:44:52 -0300

    Create update_ubuntu_part2.sh

---
+++ update_ubuntu_part2.sh
@@ -0,0 +1,10 @@
+
+echo " ------------------------------------------"
+echo " ------npm - bower - ionic - cordova ------"
+echo " ------------------------------------------"
+
+nvm install v7.10.1
+nvm alias default v7.10.1
+npm install bower ionic cordova -g
+
+
