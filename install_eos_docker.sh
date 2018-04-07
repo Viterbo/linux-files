c25807f120285e30334167d3b7f4975c5df3dbbf
Viterbo <Viterbo@users.noreply.github.com>
2018-04-07 20:35:18 -0300

    Update install_eos_docker.sh

--- install_eos_docker.sh
+++ install_eos_docker.sh
@@ -12,3 +12,18 @@ echo " ----- Running Docker EOS Container -------"
 echo " ------------------------------------------"
 cd eos/Docker
 docker build . -t eosio/eos
+
+
+echo " ------------------------------------------"
+echo " ----- IF ERROR: permision denied ---------"
+echo " ------------------------------------------"
+echo "type:"
+echo "   sudo usermod -a -G docker $USER"
+echo "then reboot"
+echo "finally login and type:"
+echo "   cd /var/www/eos/Docker && docker build . -t eosio/eos"
+
+
+
+
+
